485722a5739e27225e7e0a3bbde325e263a06d1a
VikasKumar8354 <120902vikaskumar@gmail.com>
2026-02-19 23:31:40 +0530

    user service update

--- src/main/java/com/servicebooking/security/JwtTokenProvider.java
+++ src/main/java/com/servicebooking/security/JwtTokenProvider.java
@@ -1,6 +1,5 @@
 package com.servicebooking.security;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
